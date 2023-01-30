b242b3f2d2b2141ca635b29a74d922f30eb9b810
Alexey <aa.gorschenko@yandex.ru>
2023-01-30 13:02:25 +0300

    Auth validation has been added

--- libs/contracts/src/lib/account/account-register.ts
+++ libs/contracts/src/lib/account/account-register.ts
@@ -1,9 +1,16 @@
+import { IsEmail, IsString } from 'class-validator'
+
 export namespace AccountRegister {
     export const topic = 'account.register.command'
 
     export class Request {
+        @IsEmail()
         email: string
+
+        @IsString()
         password: string
+        
+        @IsString()
         displayName?: string
     }
     
