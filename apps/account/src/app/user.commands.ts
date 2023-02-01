b39a18979c57224b9a9f978e2d688131305e4bed
Alexey <aa.gorschenko@yandex.ru>
2023-02-01 22:30:43 +0300

    Account commands have been edited

--- apps/account/src/app/user.commands.ts
+++ apps/account/src/app/user.commands.ts
@@ -1,13 +1,15 @@
 import { Body, Controller } from '@nestjs/common';
 import { AccountBuyCourse, AccountChangeProfile, AccountCheckPayment } from '@services/contracts';
-import { RMQValidate, RMQRoute } from 'nestjs-rmq';
+import { RMQValidate, RMQRoute, RMQService } from 'nestjs-rmq';
 import { UserEntity } from './user/entities/user.entity';
 import { UserRepository } from './user/repositories/user.repository';
+import { BuyCourseSaga } from './user/sagas/buy-course.saga';
 
 @Controller()
 export class UserCommands {
     constructor(
-        public readonly userRepository: UserRepository
+        public readonly userRepository: UserRepository,
+        private readonly rmqService: RMQService,
     ) {}
 
     @RMQValidate()
@@ -25,12 +27,28 @@ export class UserCommands {
     @RMQValidate()
     @RMQRoute(AccountBuyCourse.topic)
     async buyCourse(@Body() { userId, courseId }: AccountBuyCourse.Request): Promise<AccountBuyCourse.Response> {
-
+        const existedUser = await this.userRepository.findById(userId)
+        if (!existedUser){
+            throw new Error('Такого пользователя нет')
+        }
+        const userEntity = new UserEntity(existedUser)
+        const saga = new BuyCourseSaga(userEntity, courseId, this.rmqService)
+        const { user, paymentLink } = await saga.getState().pay()
+        await this.userRepository.updateUser(user)
+        return { paymentLink }
     }
 
     @RMQValidate()
     @RMQRoute(AccountCheckPayment.topic)
     async checkPayment(@Body() { userId, courseId }: AccountCheckPayment.Request): Promise<AccountCheckPayment.Response> {
-
+        const existedUser = await this.userRepository.findById(userId)
+        if (!existedUser){
+            throw new Error('Такого пользователя нет')
+        }
+        const userEntity = new UserEntity(existedUser)
+        const saga = new BuyCourseSaga(userEntity, courseId, this.rmqService)
+        const { user, status } = await saga.getState().checkPayment()
+        await this.userRepository.updateUser(user)
+        return { status }
     }
 }
--- apps/account/src/app/user/sagas/buy-course.steps.ts
+++ apps/account/src/app/user/sagas/buy-course.steps.ts
@@ -1,4 +1,4 @@
-import { CourseGetCourse, PaymentCheck, PaymentGenerateLink } from '@services/contracts';
+import { CourseGetCourse, PaymentCheck, PaymentGenerateLink, PaymentStatus } from '@services/contracts';
 import { PurchaseState } from '@services/interfaces';
 import { UserEntity } from '../entities/user.entity';
 import { BuyCourseSagaState } from './buy-course.state';
@@ -23,7 +23,7 @@ export class BuyCourseSagaStateStarted extends BuyCourseSagaState {
         this.saga.setState(PurchaseState.WaitingForPayment, course._id)
         return { paymentLink, user: this.saga.user }
     }
-    public checkPayment(): Promise<{ user: UserEntity }> {
+    public checkPayment(): Promise<{ user: UserEntity, status: PaymentStatus }> {
         throw new Error('Нельзя проверить платеж, который не начался')
     }
     public async cancel(): Promise<{ user: UserEntity }> {
@@ -33,49 +33,49 @@ export class BuyCourseSagaStateStarted extends BuyCourseSagaState {
 }
 
 export class BuyCourseSagaStateWaitingForPayment extends BuyCourseSagaState {
-    public pay(): Promise<{ paymentLink: string; user: UserEntity; }> {
+    public pay(): Promise<{ paymentLink: string, user: UserEntity }> {
         throw new Error('Нельзя создать ссылку на оплату в процессе');
     }
-    public async checkPayment(): Promise<{ user: UserEntity; }> {
+    public async checkPayment(): Promise<{ user: UserEntity, status: PaymentStatus }> {
         const { status } = await this.saga.rmqService.send<PaymentCheck.Request, PaymentCheck.Response>(PaymentCheck.topic, {
             userId: this.saga.user._id,
             courseId: this.saga.courseId,
         })
         if (status === 'canceled') {
             this.saga.setState(PurchaseState.Canceled, this.saga.courseId)
-            return { user: this.saga.user }
+            return { user: this.saga.user, status: 'canceled' }
         }
         if (status !== 'success') {
-            return { user: this.saga.user }
+            return { user: this.saga.user, status: 'success' }
         }
         this.saga.setState(PurchaseState.Purchased, this.saga.courseId)
     }
-    public cancel(): Promise<{ user: UserEntity; }> {
+    public cancel(): Promise<{ user: UserEntity, status: 'progress' }> {
         throw new Error('Нельзя отменить платеж в процессе');
     }
 }
 
 export class BuyCourseSagaStatePurchased extends BuyCourseSagaState {
-    public pay(): Promise<{ paymentLink: string; user: UserEntity; }> {
+    public pay(): Promise<{ paymentLink: string, user: UserEntity }> {
         throw new Error('Нельзя оплатить выбранный курс');
     }
-    public async checkPayment(): Promise<{ user: UserEntity; }> {
+    public async checkPayment(): Promise<{ user: UserEntity, status: PaymentStatus }> {
         throw new Error('Нельзя проверить платеж по купленному курсу');
     }
-    public cancel(): Promise<{ user: UserEntity; }> {
+    public cancel(): Promise<{ user: UserEntity }> {
         throw new Error('Нельзя отменить купленный курс');
     }
 }
 
 export class BuyCourseSagaStateCanceled extends BuyCourseSagaState {
-    public pay(): Promise<{ paymentLink: string; user: UserEntity; }> {
+    public pay(): Promise<{ paymentLink: string, user: UserEntity }> {
         this.saga.setState(PurchaseState.Started, this.saga.courseId)
         return this.saga.getState().pay()
     }
-    public async checkPayment(): Promise<{ user: UserEntity; }> {
+    public async checkPayment(): Promise<{ user: UserEntity, status: PaymentStatus }> {
         throw new Error('Нельзя проверить платеж по отмененному курсу');
     }
-    public cancel(): Promise<{ user: UserEntity; }> {
+    public cancel(): Promise<{ user: UserEntity }> {
         throw new Error('Нельзя отменить отмененный курс');
     }
 }
