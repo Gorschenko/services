6223348aeec4c56b343bdc360faad84206e21781
Alexey <aa.gorschenko@yandex.ru>
2023-02-11 13:59:28 +0300

    Course. Configs have been added

--- apps/course/src/app/app.module.ts
+++ apps/course/src/app/app.module.ts
@@ -1,11 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { RMQModule } from 'nestjs-rmq/dist/rmq.module';
 
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { getMongoConfig } from './configs/mongo.config';
+import { getRMQConfig } from './configs/rmq.config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: 'envs/.course.env'
+    }),
+    RMQModule.forRootAsync(getRMQConfig()),
+    MongooseModule.forRootAsync(getMongoConfig(),)
+  ],
 })
 export class AppModule {}
---
+++ apps/course/src/app/configs/mongo.config.ts
@@ -0,0 +1,27 @@
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModuleAsyncOptions } from '@nestjs/mongoose';
+
+export const getMongoConfig = (): MongooseModuleAsyncOptions => {
+    return {
+        useFactory: (configService: ConfigService) => {
+           return {
+                uri: getMongoString(configService),
+            }
+        },
+        inject: [ConfigService],
+        imports: [ConfigModule],
+    }
+}
+
+const getMongoString = (configService: ConfigService): string => {
+    return 'mongodb://' +
+        configService.get('MONGO_LOGIN') +
+        ':' +
+        configService.get('MONGO_PASSWORD') +
+        '@' +
+        configService.get('MONGO_HOST') +
+        ':' +
+        configService.get('MONGO_PORT') +
+        '/' +
+        configService.get('MONGO_AUTHDATABASE')
+}
