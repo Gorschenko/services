628ee048e65849576501ce48491a9138663e2222
Alexey <aa.gorschenko@yandex.ru>
2023-02-14 22:08:27 +0300

    Api. Get course method has been added

--- apps/api/src/app/controllers/course.controller.ts
+++ apps/api/src/app/controllers/course.controller.ts
@@ -1,9 +1,10 @@
-import { BadRequestException, Body, Controller, Post, UseGuards, UsePipes, ValidationPipe } from '@nestjs/common';
-import { CourseCreateCourse } from '@services/contracts';
+import { BadRequestException, Body, Controller, Get, Param, Post, UseGuards, UsePipes, ValidationPipe } from '@nestjs/common';
+import { CourseCreateCourse, CourseGetCourse } from '@services/contracts';
 import { RMQService } from 'nestjs-rmq';
 import { CreateCourseDto } from '../dtos/course/create-course.dto';
 import { JwtAuthGuard } from '../guards/jwt.guard';
 import { UserId } from '../guards/user.decorator';
+import { IdValidationPipe } from '../pipes/id-validation.pipe';
 
 
 
@@ -19,6 +20,18 @@ export class CourseController {
     async createCourse(@Body() dto: CreateCourseDto, @UserId() userId: string): Promise<CourseCreateCourse.Response> {
         try {
             return this.rmqService.send<CourseCreateCourse.Request, CourseCreateCourse.Response>(CourseCreateCourse.topic, { ...dto, ownerId: userId })
+        } catch (e) {
+            if (e instanceof Error) {
+                throw new BadRequestException(e.message)
+            }
+        }
+    }
+
+
+    @Get(':courseId')
+    async getCourse(@Param('courseId', IdValidationPipe) courseId: string): Promise<CourseCreateCourse.Response> {
+        try {
+            return this.rmqService.send<CourseGetCourse.Request, CourseGetCourse.Response>(CourseGetCourse.topic, { id: courseId })
         } catch (e) {
             if (e instanceof Error) {
                 throw new BadRequestException(e.message)
